4fcaa3dc08abdf79d4be27d0d860d5ed8355e0f3
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-03-18 21:42:27 +0900

    Create stockPrice.cpp
    Programmers

---
+++ Programmers/stockPrice.cpp
@@ -0,0 +1,24 @@
+//프로그래머스 Level2 주식가격 문제풀이
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size());
+    stack<int> st;
+
+    for(int i=0;i<prices.size();i++){
+        while(!st.empty() && prices[st.top()]>prices[i]){
+            answer[st.top()] = i-st.top();
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    while(!st.empty()){
+        answer[st.top()] = prices.size() - st.top() - 1;
+        st.pop();
+    }
+    return answer;
+}
